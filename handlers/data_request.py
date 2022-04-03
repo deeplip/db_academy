30e5362d124c3897cd699bcba1499e6ae9080c0e
Michal Jakubowski <michaljakubowski0@protonmail.com>
2022-04-03 19:54:16 +0200

    minor changes

--- handlers/data_request.py
+++ handlers/data_request.py
@@ -1,13 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from handlers.delay import normal_delay
 
 class DataRequest:
     def __init__(self, url, header, proxy_ip):
         self.url = url
         self.header = header
         self.proxy_ip = proxy_ip
-        self.raw_data = requests.get(self.url, headers=self.header, proxies= {"http": self.proxy_ip})
+        self.raw_data = self.make_request()
+
+    @normal_delay
+    def make_request(self):
+        return requests.get(self.url, headers=self.header, proxies={"http": self.proxy_ip})
 
     @property
     def get_soup(self):
