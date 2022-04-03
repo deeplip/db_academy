84f92bfa5eab4d36ef23ff3eeee2e1d77b5d2ab9
Michal Jakubowski <michaljakubowski0@protonmail.com>
2022-04-03 11:45:21 +0200

    add basic proxies class

---
+++ handlers/proxies.py
@@ -0,0 +1,23 @@
+import requests
+import numpy as np
+import pandas as pd
+
+class Proxies:
+    url = "https://free-proxy-list.net/"
+    header = {
+      "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+      "X-Requested-With": "XMLHttpRequest"
+    }
+
+    def __init__(self):
+        self.prox_req = self.reset_ip_list()
+
+    def reset_ip_list(self):
+        return requests.get(Proxies.url, headers=Proxies.header)
+
+    def _list(self):
+        ips_df =  pd.read_html(self.prox_req.text)[0][['IP Address', 'Port']]
+        return ips_df['IP Address'] + ':' + ips_df['Port'].astype('str')
+
+    def get_new_ip(self):
+        return np.random.choice(self._list())
